27d9555d77c30f6128480c4e613d474862a1f776
Kerem <keremkys16@gmail.com>
2024-03-10 17:03:47 +0300

    init

---
+++ app/src/main/java/com/mfstdio/calculator/CalculatorView.kt
@@ -0,0 +1,15 @@
+package com.mfstdio.calculator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CalculatorView() {
+    
+}
+
+@Preview
+@Composable
+private fun CalculatorPreview() {
+    
+}
